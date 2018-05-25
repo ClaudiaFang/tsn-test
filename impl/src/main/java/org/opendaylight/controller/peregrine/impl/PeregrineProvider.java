4c28662e35db75d9f3d75d43cd1803a451e9ea0c
ClaudiaFang <ClaudiaFang@itri.org.tw>
2018-05-25 09:19:28 +0000

    Test rest api adn fix bugs for impl.

--- impl/src/main/java/org/opendaylight/controller/peregrine/impl/PeregrineProvider.java
+++ impl/src/main/java/org/opendaylight/controller/peregrine/impl/PeregrineProvider.java
@@ -7,17 +7,61 @@
  */
 package org.opendaylight.controller.peregrine.impl;
 
+import com.google.common.util.concurrent.Futures;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+
+import org.opendaylight.yang.gen.v1.urn.ieee.std._802._1q.yang.ieee802.dot1q.cc.status.rev180215.groupinterfaceconfiguration.InterfaceList;
+import org.opendaylight.yang.gen.v1.urn.ieee.std._802._1q.yang.ieee802.dot1q.cc.status.rev180215.groupinterfaceconfiguration.InterfaceListBuilder;
+import org.opendaylight.yang.gen.v1.urn.ieee.std._802._1q.yang.ieee802.dot1q.cc.status.rev180215.groupinterfaceconfiguration.interfacelist.ConfigList;
+import org.opendaylight.yang.gen.v1.urn.ieee.std._802._1q.yang.ieee802.dot1q.cc.status.rev180215.groupinterfaceconfiguration.interfacelist.ConfigListBuilder;
+import org.opendaylight.yang.gen.v1.urn.ieee.std._802._1q.yang.ieee802.dot1q.cc.status.rev180215.groupinterfaceconfiguration.interfacelist.ConfigListKey;
+import org.opendaylight.yang.gen.v1.urn.ieee.std._802._1q.yang.ieee802.dot1q.cc.status.rev180215.groupstatus.AccumulatedLatency;
+import org.opendaylight.yang.gen.v1.urn.ieee.std._802._1q.yang.ieee802.dot1q.cc.status.rev180215.groupstatus.AccumulatedLatencyBuilder;
+import org.opendaylight.yang.gen.v1.urn.ieee.std._802._1q.yang.ieee802.dot1q.cc.status.rev180215.groupstatus.FailedInterfaces;
+import org.opendaylight.yang.gen.v1.urn.ieee.std._802._1q.yang.ieee802.dot1q.cc.status.rev180215.groupstatus.FailedInterfacesBuilder;
+import org.opendaylight.yang.gen.v1.urn.ieee.std._802._1q.yang.ieee802.dot1q.cc.status.rev180215.groupstatus.FailedInterfacesKey;
+import org.opendaylight.yang.gen.v1.urn.ieee.std._802._1q.yang.ieee802.dot1q.cc.status.rev180215.groupstatus.InterfaceConfiguration;
+import org.opendaylight.yang.gen.v1.urn.ieee.std._802._1q.yang.ieee802.dot1q.cc.status.rev180215.groupstatus.InterfaceConfigurationBuilder;
+import org.opendaylight.yang.gen.v1.urn.ieee.std._802._1q.yang.ieee802.dot1q.cc.status.rev180215.groupstatus.StatusInfo;
+import org.opendaylight.yang.gen.v1.urn.ieee.std._802._1q.yang.ieee802.dot1q.cc.status.rev180215.groupstatus.StatusInfoBuilder;
+import org.opendaylight.yang.gen.v1.urn.ieee.std._802._1q.yang.ieee802.dot1q.cc.status.rev180215.groupstatus.StreamID;
+import org.opendaylight.yang.gen.v1.urn.ieee.std._802._1q.yang.ieee802.dot1q.cc.status.rev180215.groupstatus.StreamIDBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.peregrine.rev180401.AddAllListenersInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.peregrine.rev180401.AddAllTalkersInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.peregrine.rev180401.GetStreamStatusOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.peregrine.rev180401.GetStreamStatusOutputBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.peregrine.rev180401.PeregrineService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.peregrine.rev180401.getstreamstatus.output.Status;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.peregrine.rev180401.getstreamstatus.output.StatusBuilder;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+import org.opendaylight.yangtools.yang.common.RpcResultBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class PeregrineProvider {
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.Future;
+
+public class PeregrineProvider
+        implements PeregrineService, AutoCloseable{
 
     private static final Logger LOG = LoggerFactory.getLogger(PeregrineProvider.class);
 
-    private final DataBroker dataBroker;
+    private DataBroker dataBroker;
 
     public PeregrineProvider(final DataBroker dataBroker) {
+
+        this.dataBroker = dataBroker;
+
+        LOG.debug("PeregrineProvider creation complete");
+    }
+
+    public PeregrineProvider() {
+
+        LOG.debug("PeregrineProvider creation complete");
+    }
+
+    public void setDataBroker(final DataBroker dataBroker) {
         this.dataBroker = dataBroker;
     }
 
@@ -26,6 +70,11 @@ public class PeregrineProvider {
      */
     public void init() {
         LOG.info("PeregrineProvider Session Initiated");
+
+        // Preconditions.checkNotNull(dataBroker, "dataBroker must be set");
+
+        // Register our MXBean.
+        // register();
     }
 
     /**
@@ -33,5 +82,81 @@ public class PeregrineProvider {
      */
     public void close() {
         LOG.info("PeregrineProvider Closed");
+
+        // Unregister our MXBean.
+        // unregister();
+    }
+
+
+    @Override
+    public Future<RpcResult<Void>> addAllListeners(AddAllListenersInput input) {
+        return Futures.immediateFuture(RpcResultBuilder.<Void>success().build());
+    }
+
+    @Override
+    public Future<RpcResult<Void>> addAllTalkers(AddAllTalkersInput input) {
+        return Futures.immediateFuture(RpcResultBuilder.<Void>success().build());
+    }
+
+    @Override
+    public Future<RpcResult<GetStreamStatusOutput>> getStreamStatus() {
+
+        StatusInfo statusInfo = new StatusInfoBuilder()
+                .setTalkerStatus(StatusInfo.TalkerStatus.Ready)
+                .setListenerStatus(StatusInfo.ListenerStatus.Ready)
+                .setFailureCode((short)1)
+                .build();
+
+        AccumulatedLatency accumulatedLatency = new AccumulatedLatencyBuilder()
+                .setAL(10L)
+                .build();
+
+        StreamID streamID = new StreamIDBuilder()
+                .setUniqueID(1233)
+                .setMacAddress("AA:BB:CC:DD:EE:FF").build();
+
+        List<FailedInterfaces> failedInterfacesList = new LinkedList<>();
+        FailedInterfaces failedInterfaces = new FailedInterfacesBuilder()
+                .setKey(new FailedInterfacesKey("AA:BB:CC:DD:EE:FF"))
+                .build();
+
+        failedInterfacesList.add(failedInterfaces);
+
+
+        List<InterfaceList> interfaceListsList = new LinkedList<>();
+
+        List<ConfigList> configListList = new LinkedList<>();
+        ConfigList configList = new ConfigListBuilder()
+                .setKey(new ConfigListKey((short)2))
+                .setIndex((short)2)
+                .build();
+        configListList.add(configList);
+
+        InterfaceList interfaceList = new InterfaceListBuilder()
+                .setConfigList(configListList)
+                .setMacAddress("AA:BB:CC:DD:EE:FF")
+                .build();
+        interfaceListsList.add(interfaceList);
+        InterfaceConfiguration interfaceConfiguration = new InterfaceConfigurationBuilder()
+                .setInterfaceList(interfaceListsList)
+                .build();
+
+
+        List<Status> statusList = new LinkedList<>();
+        Status status = new StatusBuilder()
+                .setStatusInfo(statusInfo)
+                .setAccumulatedLatency(accumulatedLatency)
+                .setFailedInterfaces(failedInterfacesList)
+                .setInterfaceConfiguration(interfaceConfiguration)
+                .setStreamID(streamID)
+                .build();
+
+        statusList.add(status);
+
+        GetStreamStatusOutput output = new GetStreamStatusOutputBuilder().setStatus(statusList).build();
+
+        LOG.debug("getStreamStatus(): return {}", output);
+
+        return RpcResultBuilder.success(output).buildFuture();
     }
 }
